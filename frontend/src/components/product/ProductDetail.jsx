7f5b610a070498f146cf0f19fac71d22b66d3a69
Abhishekrao980 <ay9802827@gmail.com>
2025-07-01 17:34:13 +0530

    Update ProductDetail.jsx

--- frontend/src/components/product/ProductDetail.jsx
+++ frontend/src/components/product/ProductDetail.jsx
@@ -6,7 +6,7 @@ import RelatedProduct from "./RelatedProduct";
 const ProductDetail = () => {
   const [product, setProduct] = useState();
   const { id } = useParams();
-  const url = "http://localhost:1000/api";
+  const url = "https://mern-e-comm-qjoq.onrender.com/api";
 
   useEffect(() => {
     const fetchProduct = async () => {
